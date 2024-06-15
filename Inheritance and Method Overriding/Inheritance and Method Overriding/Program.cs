59120f9a92e38685ed50bf382424fb915fe2a675
Paps004 <spsetutsi@st.ug.edu.gh>
2024-06-15 07:19:45 -1200

    Assignment

---
+++ Inheritance and Method Overriding/Inheritance and Method Overriding/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_and_Method_Overriding
+{
+    using System;
+
+    // Base class Animal
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class Dog
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class Cat
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of Animal, Dog, and Cat
+            Animal animal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            // Call the MakeSound() method on each instance
+            Console.WriteLine("Animal makes a sound:");
+            animal.MakeSound();
+
+            Console.WriteLine("Dog makes a sound:");
+            dog.MakeSound();
+
+            Console.WriteLine("Cat makes a sound:");
+            cat.MakeSound();
+        }
+    }
+    
+    }
+
