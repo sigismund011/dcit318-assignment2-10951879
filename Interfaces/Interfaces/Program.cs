e58726f7b26fdab6e2b72495fe451caf49244a10
Paps004 <spsetutsi@st.ug.edu.gh>
2024-06-15 07:35:36 -1200

    Interfaces

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+// Interface IMovable
+public interface IMovable
+{
+    void Move();
+}
+
+// Class Car that implements IMovable
+public class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+// Class Bicycle that implements IMovable
+public class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create instances of Car and Bicycle
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+
+        // Call the Move() method on each instance
+        car.Move();
+        bicycle.Move();
+    }
+}
